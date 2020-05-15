8915335d80a5b2457b1f9afe357db2eca872e81c
xMMB <60564146+xMMB@users.noreply.github.com>
2020-05-16 02:46:39 +0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,310 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+
+		<Header
+			:title="board.title"
+			@edit-board-title="openInpModal"
+			@add-board-col="openInpModal"
+		/>
+
+		<main class="board-cols-wrapper">
+			<div class="board-cols">
+				<BoardCol
+					v-for="(col, idx) in board.cols"
+					:key="col.boardId"
+					:col-data="col"
+					:i="idx"
+					:status="board.statuses"
+					@del-col="delCol"
+					@edit-col="editCol"
+					@add-card="addCard"
+					@open-desc-modal="openDescModal"
+				/>
+			</div>
+		</main>
+
+		<InputModal
+			:is-active="inpModal.active"
+			@canceled="inpModalCancel"
+			@confirmed="inpModalConfirm"
+			:title="inpModal.title"
+		>
+			<md-field
+				:class="{ 'md-invalid': inpModal.invalid }"
+			>
+				<label>{{ inpModal.label }}</label>
+				<md-input
+					v-model.trim="inpModal.val"
+					@keydown.enter="inpModalConfirm"
+					ref="modalInput"
+				></md-input>
+				<span class="md-error">{{ inpModal.error }}</span>
+			</md-field>
+		</InputModal>
+
+		<CardDescModal
+			:is-active="descModal.active"
+			:desc="descModal.desc"
+			:statuses="board.statuses"
+			@close-desc-modal="toggleDescModal"
+			@del-list-item="delListItem"
+			@del-card="delCard"
+			@save-card-status="saveCardStatus"
+			@add-check-list="addCheckList"
+			@edit-desc="editDesc"
+			@edit-term="editTerm"
+		/>
+
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// EXAMPLE DATA
+import board from './data/example.js';
+
+import InpModal from './mixins/inp-modal.js';
+import DescModal from './mixins/desc-modal.js';
+
+import Header from './components/Header/Header.vue';
+import BoardCol from './components/BoardCol/BoardCol.vue';
+import InputModal from './components/Modal/InputModal.vue';
+import CardDescModal from './components/Modal/CardDescModal.vue';
+
+let
+	curId = 1;
+
+board.cols.forEach(( col ) => {
+
+	col.boardId = curId++;
+
+	for (let i = 0; i < col.cards.length; i++)
+	{
+		col.cards[i].boardId = curId++;
+	}
+
+});
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Header,
+		BoardCol,
+		InputModal,
+		CardDescModal
+	},
+	mixins: [
+		InpModal,
+		DescModal
+	],
+	data()
+	{
+		return {
+			// selectedDate: '14.06.2020',
+			board: JSON.parse(JSON.stringify(board)),
+			curColEditIdx: 0
+		}
+	},
+	methods: {
+		editBoardTitle( val )
+		{
+			this.board.title = val;
+		},
+		addBoardCol( val )
+		{
+			this.board.cols.push({
+				boardId: ++curId,
+				title: val,
+				cards: []
+			});
+		},
+		delCol( idx )
+		{
+			this.board.cols.splice(idx, 1);
+		},
+		openDescModal( boardId )
+		{
+			let
+				foundCard = this.findCard(boardId);
+
+			if ( foundCard )
+				this.editDescModal(foundCard);
+
+			if ( boardId == this.descModal.desc.boardId )
+				this.toggleDescModal();
+		},
+		delListItem ( arr )
+		{
+			let
+				foundCard = this.findCard(arr[0]);
+
+			foundCard.list.splice(arr[1], 1);
+
+			this.editDescModal(foundCard);
+		},
+		findCard( boardId )
+		{
+			for (let i = 0; i < this.board.cols.length; i++)
+			{
+				let
+					curCol = this.board.cols[i],
+					found  = curCol.cards.find((card, idx) => {
+
+						if ( card.boardId == boardId )
+						{
+							card.idx = idx;
+
+							return true;
+						}
+
+					});
+
+				if ( found )
+				{
+					found.parent = curCol.title;
+					found.colIdx = i;					
+
+					return found;
+				}
+			}
+
+			return false;		
+		},
+		delCard( boardId )
+		{
+			let
+				delElem = this.findCard(boardId);
+
+			this.toggleDescModal();
+
+			if ( delElem !== false )
+				this.board.cols[delElem.colIdx].cards.splice(delElem.idx, 1);
+		},
+		saveCardStatus( cardData )
+		{
+			let
+				saveElem = this.findCard(cardData.boardId);
+
+			if ( saveElem !== false )
+			{
+				if ( this.board.statuses.length == cardData.status )
+					saveElem.status = false;
+				else
+					saveElem.status = cardData.status;
+
+				this.editDescModal(saveElem);
+			}
+		},
+		addCheckList( checkListData )
+		{
+			let
+				card = this.findCard(checkListData.boardId);
+
+			if ( card !== false )
+			{
+				card.list.push({
+					text: checkListData.val,
+					status: false
+				});
+
+				this.editDescModal(card);
+			}
+		},
+		editDesc( descData )
+		{
+			let
+				desc = this.findCard(descData.boardId);
+
+			if ( desc !== false )
+			{
+				desc.desc = descData.val;
+			}
+		},
+		editTerm( termData )
+		{
+			let
+				term = this.findCard(termData.boardId);
+
+			if ( term !== false )
+			{
+				if ( termData.val )
+					term.term = termData.val;
+				else
+					term.term = '';
+
+				this.editDescModal(term);
+			}				
+		},
+		editCol( idxOrVal )
+		{
+			if ( idxOrVal.constructor.name === 'Number' )
+			{
+				this.curColEditIdx = idxOrVal;
+
+				this.openInpModal({
+					val: this.board.cols[idxOrVal].title,
+					target: 'editCol',
+					title: 'Переименование колонки',
+					label: 'Введите название'
+				});
+
+				return;
+			}
+
+			this.board.cols[this.curColEditIdx].title = idxOrVal;
+		},
+		addCard ( idxOrVal )
+		{
+			if ( idxOrVal.constructor.name === 'Number' )
+			{
+				this.curColEditIdx = idxOrVal;
+
+				this.openInpModal({
+					target: 'addCard',
+					title: 'Новая карточка',
+					label: this.board.cols[idxOrVal].title
+				});
+
+				return;
+			}
+
+			this.board.cols[this.curColEditIdx].cards.push({
+				boardId: ++curId,
+				title: idxOrVal,
+				desc: '',
+				list: [],
+				term: '',
+				status: false
+			});
+		}
+	}
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	
+@import url("./assets/css/material-icons.css");
+
+.board-cols { 
+	display: flex;
+	align-items: flex-start;
+	padding: 10px;
 }
+
+.board-cols-wrapper { 
+	max-width: 100%;
+	overflow: auto;
+}
+
+@media screen and (max-width: 480px) {
+
+	.board-cols {
+		flex-direction: column;
+		align-items: center;
+		padding: 10px 0;
+	}
+
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,50 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(VueMaterial);
+
+const curVueApp = new Vue({
+    components: { App },
+    template: '<App/>',
+    render: h => h(App)
+}).$mount('#app');
+
+curVueApp.__proto__.$material.locale = {
+	startYear: 2020,
+	endYear: 2050,
+	dateFormat: 'dd.MM.yyyy',
+	days: [
+		'Воскресенье', 'Понедельник', 'Вторник',
+		'Среда', 'Четверг', 'Пятница', 'Суббота'
+	],
+	shortDays: [
+		'Вск', 'Пнд', 'Втр', 'Сре',
+		'Чтв', 'Птн', 'Суб'
+	],
+	shorterDays: [
+		'Вс', 'Пн', 'Вт', 'Ср',
+		'Чт', 'Пт', 'Сб'
+	],
+	months: [
+		'Январь', 'Февраль', 'Март',
+		'Апрель', 'Май', 'Июнь',
+		'Июль', 'Август', 'Сентябрь',
+		'Октябрь', 'Ноябрь', 'Декабрь'
+	],
+	shortMonths: [
+		'Янв.', 'Фев.', 'Мрт.', 'Апр.',
+		'Май', 'Июн.', 'Июл.', 'Авг.',
+		'Сен.', 'Окт.', 'Ноя.', 'Дек.'
+	],
+	shorterMonths: [
+		'Я', 'Ф', 'М', 'А', 'М', 'И',
+		'И', 'А', 'С', 'О', 'Н', 'Д'
+	],
+	firstDayOfAWeek: 1
+};
